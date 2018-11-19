176909e4d2223e388cac5a490e6e890acc963a77
Admin <admin@Qwertys-iMac.local>
2018-11-18 17:00:16 -0800

    type" feat |added l[ayHur tfx, plu more platforms

--- new-version-242/Assets/Scripts/playerHealth.cs
+++ new-version-242/Assets/Scripts/playerHealth.cs
@@ -3,48 +3,57 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class playerHealth : MonoBehaviour {
+public class playerHealth : MonoBehaviour
+{
+
+
     //HUD variables
+
     public Slider healthSlider;
 
     //calculat health variables
+
     public float fullHealth;
+
     float currentHealth;
+
     public GameObject deathFX;
+    public GameObject hurtFX;
 
     playerController controlMovement;
-    public int levelBoundary; // [RC] Found to be -17 on the current level
+
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         currentHealth = fullHealth;
         controlMovement = GetComponent<playerController>();
 
         healthSlider.maxValue = fullHealth;
         healthSlider.value = fullHealth;
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        // [RC] Kill player if they go out of bounds
-        if (gameObject.transform.transform.position.y <= levelBoundary && currentHealth > 0)
-        {
-            currentHealth = 0;
-            makeCharacterDead();
-        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
     }
 
 
-    public void addDamage(float damage){
-        if(damage<= 0){
+    public void addDamage(float damage)
+    {
+        if (damage <= 0)
+        {
             return;
         }
         currentHealth = currentHealth - damage;
         healthSlider.value = currentHealth;
+        Instantiate(hurtFX, transform.position, transform.rotation);
 
 
-        if(currentHealth <= 0 ){
+        if (currentHealth <= 0)
+        {
 
             Debug.Log("In current health less then script");
             makeCharacterDead();
@@ -53,7 +62,8 @@ public class playerHealth : MonoBehaviour {
         }
     }
 
-    public void makeCharacterDead(){
+    public void makeCharacterDead()
+    {
         Instantiate(deathFX, transform.position, transform.rotation);
         Destroy(gameObject);
         Debug.Log("In Make Dead");
