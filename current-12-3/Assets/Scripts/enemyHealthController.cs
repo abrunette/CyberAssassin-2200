8c4e67305a77ba03e622092f7174b969efb89949
Admin <admin@Qwertys-iMac.local>
2018-12-03 19:14:11 -0800

    type: feat |added helath pick up, coin for scoring, score system, prefabed rooks, different enemy level

---
+++ current-12-3/Assets/Scripts/enemyHealthController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyHealthController : MonoBehaviour {
+
+    public float enemyMaxHealth;
+
+    public int scoreValue = 10;
+
+
+    float currentHealth;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        currentHealth = enemyMaxHealth;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public void addDamage(float damage)
+    {
+        currentHealth -= damage; 
+
+        if (currentHealth <= 0)
+        {
+            makeDead();
+        }
+    }
+
+    void makeDead()
+    {
+        ScoreManager.score += scoreValue;
+        Destroy(gameObject);
+    }
+}
